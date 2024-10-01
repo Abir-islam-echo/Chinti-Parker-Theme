6ade23255e657dd677d74b28d7808c8013c7627a
Abir-islam-echo <abir@echologyx.com>
2024-10-01 19:31:43 +0600

    first commit

--- assets/wishlist-king.js
+++ assets/wishlist-king.js
@@ -11,8 +11,8 @@ export const createApp = ({ settings, config, locale }) => {
     locale,
     dependencies: [
       "https://cdn.jsdelivr.net/npm/@appmate/wishlist@4.17.6/components/wishlist.css",
-      "//www.chintiandparker.com/cdn/shop/t/302/assets/wishlist-king-custom.css?v=63816522155686180531721142195",
-      "//www.chintiandparker.com/cdn/shop/t/302/assets/wishlist-king-components.js?v=67192780879355402051721142194",
+      "//www.chintiandparker.com/cdn/shop/t/311/assets/wishlist-king-custom.css?v=63816522155686180531727784570",
+      "//www.chintiandparker.com/cdn/shop/t/311/assets/wishlist-king-components.js?v=67192780879355402051727784569",
     ],
   });
 
